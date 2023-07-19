75be4816d817a0e0aabb0f0178c9a3a9e994451c
minister <ministerhussein@gmail.com>
2023-07-19 17:59:23 +0100

    ref: branching

--- selenium/cookieclicker.py
+++ selenium/cookieclicker.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import datetime
 
 chrome_driver_path  = "c:/Users/MINISTER/Documents/chromedriver.exe"
 services = Service(executable_path= chrome_driver_path)
@@ -11,31 +12,62 @@ driver = webdriver.Chrome(service=services, options=option)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/ ")
 
-# get cookie element
+# # get cookie element
+# cookie = driver.find_element(By.ID, "cookie")
+
+# # five seconds 
+# five_seconds = time.time() + 5
+
+# # five minutes
+# five_minutes = time.time() + (5 * 60)
+
+
+# while True:
+#     cookie.click()
+
+#     # affordable purchase
+#     if time.time() > five_seconds:
+
+#         affordable_purchase = driver.find_elements(By.CSS_SELECTOR, '#store b')
+#         print(affordable_purchase.text)
+# #amount of money
+
+#     avail_money = driver.find_element(By.ID, 'money')
+#     print(avail_money.text)
+#     break
+
+#this was my final code import ti
+
+TIMER_DURATION = (5*60)  # in seconds #
+
+# ELEMENTS #
 cookie = driver.find_element(By.ID, "cookie")
+store_ids = ["buyCursor", "buyGrandma", "buyFactory"]
 
-# five seconds 
-five_seconds = time.time() + 5
+# TIMER #
+timeout = time.time() + (TIMER_DURATION*60)
 
-# five minutes
-five_minutes = time.time() + (5 * 60)
+# MAKE PURCHASES #
+purchases = []
 
+def purchase_engine():
+    """checks which items are available and buys the most expensive item"""
+    for id in store_ids[::-1]:
+        item = driver.find_element(By.ID, id)
+        if item.get_attribute("class") == "" and id not in purchases:
+            item.click()
+            purchases.append(id)
+            break
+        elif len(purchases) >= len(store_ids) and item.get_attribute("class") == "":
+             item.click()
+             break
 
-while True:
+# GAMEPLAY LOOP #
+while time.time() <= timeout:
     cookie.click()
-
-    # affordable purchase
-    if time.time() > five_seconds:
-
-        affordable_purchase = driver.find_elements(By.CSS_SELECTOR, '#store b')
-        print(affordable_purchase.text)
-#amount of money
-
-    avail_money = driver.find_element(By.ID, 'money')
-    print(avail_money.text)
-    break
-
-
-
-input("The cookie clicker project")
-
+    if datetime.datetime.now().second % 5 == 0:
+        purchase_engine()
+cps = driver.find_element(By.ID, "cps").text
+driver.quit()
+print("Time is up.")
+print(cps)
